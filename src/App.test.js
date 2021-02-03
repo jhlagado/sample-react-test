a17c90d54ad37843441a4a45bd614fc2b9181897
John Hardy <jh@lagado.com>
2021-02-03 12:09:19 +1100

    save

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import { App } from "./App";
 
@@ -9,4 +9,15 @@ test("renders the correct content", () => {
   getByText("TODOS");
   getByLabelText("What needs to be done?");
   getByText("Add #1");
+});
+
+x test("Allow users to add items to their list", () => {
+  const { getByText, getByLabelText } = render(<App />);
+
+  const input = getByLabelText("What needs to be done?");
+  fireEvent.change(input, {target:{value: "RTL Presentaion Slides"}});
+  fireEvent.click(getByText("Add #1"));
+
+  getByText("RTL Presentaion Sl ides");
+  getByText("Add #2");
 });
